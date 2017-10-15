cab63f43e5a0ffaedb7a5553a5d50eddcbe27ea8
Jordan Demaison <jordan.demaison@gmail.com>
2017-10-15 18:46:28 +0200

    Added db connection

--- extranet/__init__.py
+++ extranet/__init__.py
@@ -1,11 +1,15 @@
 # import dependencies
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 # create WSGI application object
 app = Flask('extranet')
 
 # load configuration
 app.config.from_object('extranet.config')
+
+# load database
+db = SQLAlchemy(app)
 
 # load modules
 from extranet.modules.auth import bp as auth_module
--- extranet/config.example.py
+++ extranet/config.example.py
@@ -2,6 +2,10 @@
 SECRET_KEY = 'Generate a 32char random string'
 SESSION_COOKIE_NAME = 'extranet_session'
 
+# database configuration
+SQLALCHEMY_DATABASE_URI = 'Database url'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 # office 365 oauth config
 OFFICE365_CLIENT_ID = 'App ID / Client Id'
 OFFICE365_CLIENT_SECRET = 'Password'
