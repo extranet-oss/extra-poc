d1ba74c8be75bd286128b486828be9c56e1d6723
Jordan Demaison <jordan.demaison@gmail.com>
2017-10-21 02:47:28 +0200

    Oauth accept/deny dialog bypassed if application already authorized

--- extranet/modules/oauthprovider/controllers/authorize.py
+++ extranet/modules/oauthprovider/controllers/authorize.py
@@ -1,9 +1,10 @@
 from flask import render_template, request, flash, session
-from flask_login import fresh_login_required
+from flask_login import fresh_login_required, current_user
 from werkzeug.security import gen_salt
 
 from extranet.modules.oauthprovider import bp, provider
 from extranet.models.oauth_app import OauthApp
+from extranet.models.oauth_token import OauthToken
 
 def render_authorize(*args, **kwargs):
   app_id = kwargs.get('client_id')
@@ -19,13 +20,18 @@ def render_authorize(*args, **kwargs):
 @fresh_login_required
 @provider.authorize_handler
 def authorize(*args, **kwargs):
+  # bypass accept/deny form if already accepted (has token)
+  if OauthToken.query.filter_by(user_id=current_user.id).first() is not None:
+    return True
+
+  # render accept/deny if GET request
   if request.method == 'GET':
     return render_authorize(**kwargs)
 
+  # verify POST request legitimacy
   if 'oauthprovider.snitch' not in session or session['oauthprovider.snitch'] != request.form.get('snitch'):
     flash('Something went wrong, please retry.')
     return render_authorize(**kwargs)
 
   confirm = request.form.get('confirm', 'no')
-
   return confirm == 'yes'
