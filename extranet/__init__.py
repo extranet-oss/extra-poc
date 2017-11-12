2221591745e929e0e1cf43cef1d59e4a839babe8
Jordan Demaison <jordan.demaison@gmail.com>
2017-11-13 00:40:25 +0100

    Crawling user data before accessing test profile page

--- extranet/__init__.py
+++ extranet/__init__.py
@@ -43,8 +43,10 @@ from flask import render_template
 @app.route('/')
 def index():
   return render_template('index.html')
-from flask_login import login_required
+from flask_login import login_required, current_user
+from extranet.crawler import user
 @app.route('/profile/')
 @login_required
 def profile():
+  user.update(current_user)
   return render_template('profile.html')
