5ce689f3cd550563ad8f9549cff8885f540eba61
Jordan Demaison <jordan.demaison@gmail.com>
2017-11-12 03:46:32 +0100

    Major user model changes
    - Now every user of the intranet is saved into the database (essential data & picture)
    - Logging in by office365 'registers' the user
    - Only registered users can use extranet
    - Unregistering will delete all user data except picture & essential data
    - No more profile model

--- extranet/models/user.py
+++ extranet/models/user.py
@@ -1,55 +1,114 @@
 import uuid
 import json
 from enum import Enum, auto
+from slugify import slugify
 
 from extranet import db
 from extranet.models._templates import Dated, Intra
 
+
+#
+# Associate user with groups
+#
 user_groups = db.Table('user_xref_user_group',
   db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
   db.Column('user_group_id', db.Integer, db.ForeignKey('user_group.id'), primary_key=True)
 )
 
 
-class User(Dated):
+#
+# User basic data
+#
+class User(Intra):
 
   # uuid
   uuid = db.Column(db.String(36), index=True, unique=True, nullable=False)
-  active = db.Column(db.Boolean, nullable=False)
+  registered = db.Column(db.Boolean, nullable=False, default=False)
+  active = db.Column(db.Boolean, nullable=False, default=True)
 
   # basic user info
-  email = db.Column(db.String(320), unique=True, nullable=False)
   username = db.Column(db.String(255), index=True, unique=True, nullable=False)
   realm = db.Column(db.String(253), nullable=False)
-  firstname = db.Column(db.String(255), index=True, nullable=False)
-  lastname = db.Column(db.String(255), index=True, nullable=False)
+  fullname = db.Column(db.String(255), index=True, nullable=False)
+  public = db.Column(db.Boolean, nullable=False, default=False)
+
+  # profile pic
+  picture_id = db.Column(db.Integer, db.ForeignKey('picture.id'))
+  picture = db.relationship('Picture', lazy=True, backref=db.backref('user', lazy=True), cascade='all, delete-orphan', single_parent=True)
+
+  # registered user info
+  rtime = db.Column(db.DateTime)
+  email = db.Column(db.String(320), unique=True)
+  firstname = db.Column(db.String(255))
+  lastname = db.Column(db.String(255))
 
   # office365 user info
   office365_uid = db.Column(db.String(36), unique=True)
   _office365_token = db.Column(db.Text, name='office365_token')
 
   # intranet user info
-  intra_uid = db.Column(db.String(320), unique=True)
+  intra_uid = db.Column(db.String(320), index=True, unique=True, nullable=False)
   intra_token = db.Column(db.String(40))
+  ctime = db.Column(db.DateTime)
+  mtime = db.Column(db.DateTime)
 
   # relations
-  profile = db.relationship('UserProfile', lazy=True, backref=db.backref('user', lazy=True), cascade='all, delete-orphan', uselist=False)
+  custom_infos = db.relationship('UserProfileCustomInfo', lazy=True, backref=db.backref('user', lazy=True), cascade='all, delete-orphan')
   groups = db.relationship('UserGroup', secondary=user_groups, lazy=True, backref=db.backref('users', lazy=True))
-  managed_groups = db.relationship('UserGroup', lazy=True, backref=db.backref('master', lazy=True))
+  managed_groups = db.relationship('UserGroup', lazy=True, backref=db.backref('master', lazy=True), cascade='all, delete-orphan')
   academic = db.relationship('UserAcademic', lazy=True, backref=db.backref('user', lazy=True), cascade='all, delete-orphan', uselist=False)
   oauth_apps = db.relationship('OauthApp', lazy=True, backref=db.backref('owner', lazy=True), cascade='all, delete-orphan')
   oauth_tokens = db.relationship('OauthToken', lazy=True, backref=db.backref('user', lazy=True), cascade='all, delete-orphan')
 
-  def __init__(self, email, firstname, lastname):
-    self.uuid = uuid.uuid4()
-    self.active = True
+  def __init__(self, login, fullname):
+    self.uuid = str(uuid.uuid4())
 
+    self.fullname = fullname
+    self.intra_uid = login
+
+    if '@' in login:
+      self.username = slugify(login.split('@')[0])
+      self.realm = login.split('@')[1]
+    else:
+      self.username = slugify(login)
+      self.realm = 'legacy'
+
+    count = 1
+    username = self.username
+    while User.query.filter_by(username=self.username).first():
+      count += 1
+      self.username = username + str(count)
+
+  def register(self, office365_uid, email, firstname, lastname):
+    self.registered = True
+
+    self.office365_uid = office365_uid
+
+    self.rtime = db.func.current_timestamp()
     self.email = email
     self.firstname = firstname
     self.lastname = lastname
+    self.fullname = firstname + ' ' + lastname
 
-    self.username = self.email.split('@')[0]
-    self.realm = self.email.split('@')[1]
+  def unregister(self):
+    self.registered = False
+    self.office365_uid = None
+    self._office365_token = None
+    self.intra_token = None
+    self.rtime = None
+    self.email = None
+    self.public = False
+
+    # delete data
+    for info in self.custom_infos:
+      db.session.delete(info)
+    if self.academic is not None:
+      db.session.delete(self.academic)
+    for app in self.oauth_apps:
+      db.session.delete(app)
+    for token in self.oauth_tokens:
+      db.session.delete(token)
+    db.session.commit()
 
   # usm stuff
   @property
@@ -80,35 +139,9 @@ class User(Dated):
     return '<User %r>' % self.id
 
 
-class UserProfile(Intra):
-
-  # owner
-  user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-  # timestamps
-  ctime = db.Column(db.DateTime, nullable=False)
-  mtime = db.Column(db.DateTime, nullable=False)
-
-  # profile access (public means profile accessible to everyone, findable on userlist)
-  public = db.Column(db.Boolean, nullable=False, default=False)
-
-  # profile pic
-  picture_id = db.Column(db.Integer, db.ForeignKey('picture.id'))
-  picture = db.relationship('Picture', lazy=True, backref=db.backref('user', lazy=True), cascade='all, delete-orphan', single_parent=True)
-
-  # custom user info
-  custom_infos = db.relationship('UserProfileCustomInfo', lazy=True, backref=db.backref('user_profile', lazy=True), cascade='all, delete-orphan')
-
-  def __init__(self, user, ctime, mtime):
-    self.user_id = user.id
-
-    self.ctime = ctime
-    self.mtime = mtime
-
-  def __repr__(self):
-    return '<UserProfile %r>' % self.id
-
-
+#
+# Custom profile infos types
+#
 class UserProfileCustomInfoTypes(Enum):
   country = auto()
   city = auto()
@@ -127,16 +160,19 @@ class UserProfileCustomInfoTypes(Enum):
   github = auto()
 
 
+#
+# User custom profile infos
+#
 class UserProfileCustomInfo(Intra):
 
-  user_profile_id = db.Column(db.Integer, db.ForeignKey('user_profile.id'), nullable=False)
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
   type = db.Column(db.Enum(UserProfileCustomInfoTypes), nullable=False)
 
   value = db.Column(db.String(255), nullable=False)
   public = db.Column(db.Boolean, nullable=False, default=True)
 
-  def __init__(self, user_profile, type, value):
-    self.user_profile_id = user_profile.id
+  def __init__(self, user, type, value):
+    self.user_id = user.id
     self.type = type
 
     self.value = value
@@ -145,6 +181,9 @@ class UserProfileCustomInfo(Intra):
     return '<UserProfileCustomInfo %r>' % self.id
 
 
+#
+# Groups
+#
 class UserGroup(Intra):
 
   # uuid
@@ -158,23 +197,25 @@ class UserGroup(Intra):
   member_count = db.Column(db.Integer, nullable=False)
 
   # group manager
-  manager_intra_uid = db.Column(db.String(320), nullable=False)
-  manager_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+  manager_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
-  def __init__(self, code, name, description, count, manager_login):
-    self.uuid = uuid.uuid4()
+  def __init__(self, code, name, description, count, manager):
+    self.uuid = str(uuid.uuid4())
     self.intra_code = code
 
     self.name = name
     self.description = description
     self.member_count = count
 
-    self.manager_intra_uid = manager_login
+    self.manager_id = manager.id
 
   def __repr__(self):
     return '<UserGroup %r>' % self.id
 
 
+#
+# User academic data
+#
 class UserAcademic(Intra):
 
   # owner
