b67e256b2690165b61d368a21a86fcc00d4e2fb6
Jordan Demaison <jordan.demaison@gmail.com>
2017-11-19 21:47:53 +0100

    Added CORS header to API

--- extranet/modules/api/v0/__init__.py
+++ extranet/modules/api/v0/__init__.py
@@ -2,6 +2,7 @@
 from flask import Blueprint, request
 
 from flask_limiter import Limiter
+from flask_cors import CORS
 from extranet import app
 
 limiter = Limiter(app)
@@ -15,6 +16,7 @@ bp = Blueprint('api_v0', __name__,
                url_prefix='/api/v0')
 
 limiter.shared_limit(app.config['RATELIMIT_API'], 'api-v0')(bp)
+CORS(bp, origins='*', send_wildcard=True)
 
 # load controllers
 import extranet.modules.api.v0.controllers
