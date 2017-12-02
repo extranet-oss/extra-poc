0289a6b193f19e22eb64ba8648d3636c6e80c796
Jordan Demaison <jordan.demaison@gmail.com>
2017-12-02 18:14:36 +0100

    Added API params proxy

--- extranet/modules/api/v0/api/utils.py
+++ extranet/modules/api/v0/api/utils.py
@@ -4,6 +4,7 @@ from werkzeug.local import LocalProxy
 from extranet.utils import external_url
 
 auth = LocalProxy(lambda: _get_auth())
+params = LocalProxy(lambda: _get_params())
 
 default_auth_data = {
     'user': None,
@@ -18,6 +19,12 @@ def _get_auth():
         return default_auth_data
 
     return getattr(_request_ctx_stack.top, 'api_auth', None)
+
+def _get_params():
+    if has_request_context() and hasattr(_request_ctx_stack.top, 'api_params'):
+        return getattr(_request_ctx_stack.top, 'api_params', None)
+
+    return None
 
 
 def api_url(endpoint, **kwargs):
